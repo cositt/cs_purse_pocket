f8840475ed341b4b4d54fdfcb19a6f23017ad289
Juan Guerrero <desarrollo@cositt.com>
2026-03-24 10:12:02 +0100

    first push

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Dev Base",
+    "version": "19.0.1.0.0",
+    "summary": "Modulo base para desarrollo local",
+    "category": "Tools",
+    "author": "CentroSanitario",
+    "license": "LGPL-3",
+    "depends": ["base"],
+    "data": [
+        "views/dev_base_views.xml"
+    ],
+    "installable": True,
+    "application": False
+}
